76d2406120da3562bac9b1f676543b682d200305
Japhet Lee Ong <japhetlee.dev@gmail.com>
2023-06-06 23:16:38 +0800

    Add project files.

---
+++ CoreImageUploader/Controllers/UploadController.cs
@@ -0,0 +1,55 @@
+﻿using CoreImageUploader.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace CoreImageUploader.Controllers
+{
+    public class UploadController : Controller
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private readonly Regex sWhitespace = new Regex(@"\s+");
+
+        public UploadController(IWebHostEnvironment hostEnvironment)
+        {
+            webHostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(UserModel _user)
+        {
+            if (ModelState.IsValid)
+            {
+                var imageDirectory = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                var name = ReplaceWhitespace(_user.Name,"");
+
+                if (!Directory.Exists(imageDirectory))
+                {
+                    Directory.CreateDirectory(imageDirectory);
+                }
+
+                var fileName = Guid.NewGuid().ToString() + "_" + name + "_" + _user.UserImage?.FileName;
+                var filePath = Path.Combine(imageDirectory, fileName);
+
+                using var stream = new FileStream(filePath, FileMode.Create);
+                _user.UserImage?.CopyTo(stream);
+                Debug.WriteLine(imageDirectory);
+                Debug.WriteLine(filePath);
+
+            }
+
+            return View();
+        }
+
+        [NonAction]
+        private string ReplaceWhitespace(string input, string replacement)
+        {
+            return sWhitespace.Replace(input, replacement);
+        }
+    }
+}
---
+++ CoreImageUploader/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreImageUploader.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; } = String.Empty;
+
+        [Required(ErrorMessage = "Image is required")]
+        public IFormFile? UserImage { get; set; }
+
+    }
+}
